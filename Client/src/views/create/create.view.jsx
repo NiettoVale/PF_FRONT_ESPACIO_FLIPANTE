417679c2778f79573fa9aa1863a99e3d80dbd78a
Ramirof <ramirofiscella@gmail.com>
2023-09-18 19:37:41 -0300

    added styles to createForm

--- Client/src/views/create/create.view.jsx
+++ Client/src/views/create/create.view.jsx
@@ -5,7 +5,6 @@ const CreateProduct = () => {
   return (
     <div className="create-product">
       <h2>Crear Nuevo Producto</h2>
-      <p> Completa los detalles del nuevo producto:</p>
       <CreateForm />
     </div>
   );
