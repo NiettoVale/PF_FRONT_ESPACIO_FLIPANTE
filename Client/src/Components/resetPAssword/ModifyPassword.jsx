2fdacee6b5528a56b89e39588e55cdca6ce696e0
Ramirof <ramirofiscella@gmail.com>
2023-09-20 17:11:56 -0300

    fixed password conflicts

--- Client/src/Components/resetPAssword/ModifyPassword.jsx
+++ Client/src/Components/resetPAssword/ModifyPassword.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./ModifyPassword.module.css";
 const back = process.env.REACT_APP_BACK;
 
 function ModifyPassword() {
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [message, setMessage] = useState(''); 
-  const correoElectronico = localStorage.getItem('correoElectronico');
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const correoElectronico = localStorage.getItem("correoElectronico");
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    if (name === 'password') {
+    if (name === "password") {
       setPassword(value);
-    } else if (name === 'confirmPassword') {
+    } else if (name === "confirmPassword") {
       setConfirmPassword(value);
     }
   };
@@ -24,7 +24,7 @@ function ModifyPassword() {
 
     if (password !== confirmPassword) {
       // Contraseñas no coinciden, muestra una alerta
-      alert('Las contraseñas no coinciden.');
+      alert("Las contraseñas no coinciden.");
       return;
     }
 
@@ -34,7 +34,7 @@ function ModifyPassword() {
 
       if (!response.ok) {
         // Correo no registrado, muestra una alerta
-        alert('El correo electrónico no está registrado.');
+        alert("El correo electrónico no está registrado.");
         return;
       }
 
@@ -43,9 +43,9 @@ function ModifyPassword() {
 
       // Realiza la solicitud PUT para cambiar la contraseña
       const putResponse = await fetch(`${back}modify-password/${data.id}`, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           newPassword: password, // Usamos la nueva contraseña del estado
@@ -57,48 +57,48 @@ function ModifyPassword() {
       }
 
       // Limpia los campos del formulario
-      setPassword('');
-      setConfirmPassword('');
-      setMessage('Contraseña cambiada con éxito.');
-      alert('Contraseña cambiada exitosamente');
-      navigate('/login');
+      setPassword("");
+      setConfirmPassword("");
+      setMessage("Contraseña cambiada con éxito.");
+      alert("Contraseña cambiada exitosamente");
+      navigate("/login");
     } catch (error) {
-      console.error('Error al realizar la solicitud:', error);
+      console.error("Error al realizar la solicitud:", error);
       // Maneja otros errores si es necesario
     }
   };
 
   return (
-    <div className={styles.container}>      
-      
+    <div className={styles.container}>
       <form onSubmit={handleSubmit} className={styles.formContainer}>
-          <h2>Cambiar Contraseña</h2>
-          <p>Correo Electrónico: {correoElectronico}</p>    
-          <label htmlFor="password">Nueva Contraseña:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-            required
-            className={styles.inputForm}
-          />       
-          <label htmlFor="confirmPassword">Confirmar Contraseña:</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            value={confirmPassword}
-            onChange={handleChange}
-            required
-            className={styles.inputForm}
-          />       
-        
-          <button type="submit" className={styles.buttonForm} >Cambiar Contraseña</button>
-          <p>{message}</p>
+        <h2>Cambiar Contraseña</h2>
+        <p>Correo Electrónico: {correoElectronico}</p>
+        <label htmlFor="password">Nueva Contraseña:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          required
+          className={styles.inputForm}
+        />
+        <label htmlFor="confirmPassword">Confirmar Contraseña:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleChange}
+          required
+          className={styles.inputForm}
+        />
+
+        <button type="submit" className={styles.buttonForm}>
+          Cambiar Contraseña
+        </button>
+        <p>{message}</p>
       </form>
-      
     </div>
   );
 }
